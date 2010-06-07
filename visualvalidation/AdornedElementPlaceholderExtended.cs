dae330812e444a9877882a2332a56d0d44b75ca0
Peter Zhao <peter.qs.zhao@gmail.com>
2010-06-07 18:26:18 +0100

    add adornedElementPlaceholderextended

---
+++ visualvalidation/AdornedElementPlaceholderExtended.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace VisualValidation
+{
+    /// <summary>
+    /// Provides an implementation of the WPF AdornedElementPlaceholder that can be used in scenarios outside of WPF's internal
+    /// validation mechanisms (the out-of-the-box AdornedElementPlaceholder relies on a TemplatedAdorner object which is an internal
+    /// class).
+    /// </summary>
+    [ContentProperty("Child")]
+    public class AdornedElementPlaceholderExtended : FrameworkElement, IAddChild
+    {
+        private UIElement _child;
+        private TemplatedAdorner _templatedAdorner;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AdornedElementPlaceholderExtended"/> class.
+        /// </summary>
+        public AdornedElementPlaceholderExtended()
+        {
+            
+        }
+
+        /// <summary>
+        /// Gets the adorned element.
+        /// </summary>
+        /// <value>The adorned element.</value>
+        public UIElement AdornedElement
+        {
+            get
+            {
+                if (TemplatedAdorner != null)
+                {
+                    return TemplatedAdorner.AdornedElement;
+                }
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the child.
+        /// </summary>
+        /// <value>The child.</value>
+        public UIElement Child
+        {
+            get { return _child; }
+            set
+            {
+                var child = _child;
+                if (child != value)
+                {
+                    RemoveVisualChild(child);
+                    RemoveLogicalChild(child);
+                    _child = value;
+                    AddVisualChild(value);
+                    AddLogicalChild(value);
+                    InvalidateMeasure();
+                }
+            }
+        }
+
+        private TemplatedAdorner TemplatedAdorner
+        {
+            get
+            {
+                if (_templatedAdorner == null)
+                {
+                    var templatedParent = TemplatedParent as FrameworkElement;
+                    if (templatedParent != null)
+                    {
+                        _templatedAdorner = VisualTreeHelper.GetParent(templatedParent) as TemplatedAdorner;
+                        if ((_templatedAdorner != null) && (_templatedAdorner.PlaceholderReferenceElement == null))
+                        {
+                            _templatedAdorner.PlaceholderReferenceElement = this;
+                        }
+                    }
+                }
+                return _templatedAdorner;
+            }
+        }
+
+        /// <summary>
+        /// Gets the number of visual child elements within this element.
+        /// </summary>
+        /// <value></value>
+        /// <returns>
+        /// The number of visual child elements for this element.
+        /// </returns>
+        protected override int VisualChildrenCount
+        {
+            get
+            {
+                if (_child != null)
+                {
+                    return 1;
+                }
+                return 0;
+            }
+        }
+
+        /// <summary>
+        /// Adds a child object.
+        /// </summary>
+        /// <param name="value">The child object to add.</param>
+        public void AddChild(object value)
+        {
+            if (value != null)
+            {
+                if (!(value is UIElement))
+                {
+                }
+                if (Child != null)
+                {
+                }
+                Child = (UIElement) value;
+            }
+        }
+
+        /// <summary>
+        /// Adds the text content of a node to the object.
+        /// </summary>
+        /// <param name="text">The text to add to the object.</param>
+        public void AddText(string text)
+        {
+        }
+
+        /// <summary>
+        /// Arranges the override.
+        /// </summary>
+        /// <param name="arrangeBounds">The arrange bounds.</param>
+        /// <returns></returns>
+        protected override Size ArrangeOverride(Size arrangeBounds)
+        {
+            var child = Child;
+            if (child != null)
+            {
+                child.Arrange(new Rect(arrangeBounds));
+            }
+            return arrangeBounds;
+        }
+
+        /// <summary>
+        /// Overrides <see cref="M:System.Windows.Media.Visual.GetVisualChild(System.Int32)"/>, and returns a child at the specified index from a collection of child elements.
+        /// </summary>
+        /// <param name="index">The zero-based index of the requested child element in the collection.</param>
+        /// <returns>
+        /// The requested child element. This should not return null; if the provided index is out of range, an exception is thrown.
+        /// </returns>
+        protected override Visual GetVisualChild(int index)
+        {
+            if ((_child == null) || (index != 0))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            return _child;
+        }
+
+        /// <summary>
+        /// Measures the override.
+        /// </summary>
+        /// <param name="constraint">The constraint.</param>
+        /// <returns></returns>
+        protected override Size MeasureOverride(Size constraint)
+        {
+            if (TemplatedParent == null)
+            {
+                throw new Exception("TemplatedParent cannot be null.");
+            }
+            if (AdornedElement == null)
+            {
+                return new Size(0.0, 0.0);
+            }
+            Size renderSize = AdornedElement.RenderSize;
+            UIElement child = Child;
+            if (child != null)
+            {
+                child.Measure(renderSize);
+            }
+            return renderSize;
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Windows.FrameworkElement.Initialized"/> event. This method is invoked whenever <see cref="P:System.Windows.FrameworkElement.IsInitialized"/> is set to true internally.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.Windows.RoutedEventArgs"/> that contains the event data.</param>
+        protected override void OnInitialized(EventArgs e)
+        {
+            if (base.TemplatedParent == null)
+            {
+            }
+            base.OnInitialized(e);
+        }
+    }
+}
---
+++ visualvalidation/TemplatedAdorner.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace VisualValidation
+{
+    /// <summary>
+    /// Provides an implementation of the WPF TemplatedAdorner class which is not internal.
+    /// </summary>
+    public sealed class TemplatedAdorner : Adorner
+    {
+        private readonly Control _child;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TemplatedAdorner"/> class.
+        /// </summary>
+        /// <param name="adornedElement">The adorned element.</param>
+        /// <param name="dataContext">The data context.</param>
+        /// <param name="adornerTemplate">The adorner template.</param>
+        public TemplatedAdorner(UIElement adornedElement, object dataContext, ControlTemplate adornerTemplate)
+            : base(adornedElement)
+        {
+            _child = new Control {Template = adornerTemplate};
+            DataContext = dataContext;
+            AddVisualChild(_child);
+        }
+
+        /// <summary>
+        /// Gets or sets the reference element.
+        /// </summary>
+        /// <value>The reference element.</value>
+        public FrameworkElement PlaceholderReferenceElement { get; set; }
+
+        /// <summary>
+        /// Gets the number of visual child elements within this element.
+        /// </summary>
+        /// <value></value>
+        /// <returns>
+        /// The number of visual child elements for this element.
+        /// </returns>
+        protected override int VisualChildrenCount
+        {
+            get { return 1; }
+        }
+
+        protected override Size ArrangeOverride(Size size)
+        {
+            Size baseSize = base.ArrangeOverride(size);
+            if (_child != null)
+            {
+                _child.Arrange(new Rect(new Point(), baseSize));
+            }
+            return baseSize;
+        }
+
+        /// <summary>
+        /// Returns a <see cref="T:System.Windows.Media.Transform"/> for the adorner, based on the transform that is currently applied to the adorned element.
+        /// </summary>
+        /// <param name="transform">The transform that is currently applied to the adorned element.</param>
+        /// <returns>A transform to apply to the adorner.</returns>
+        public override GeneralTransform GetDesiredTransform(GeneralTransform transform)
+        {
+            if (PlaceholderReferenceElement == null)
+            {
+                return transform;
+            }
+            var group = new GeneralTransformGroup();
+            group.Children.Add(transform);
+            var transform2 = TransformToDescendant(PlaceholderReferenceElement);
+            if (transform2 != null)
+            {
+                group.Children.Add(transform2);
+            }
+            return group;
+        }
+
+        /// <summary>
+        /// Overrides <see cref="M:System.Windows.Media.Visual.GetVisualChild(System.Int32)"/>, and returns a child at the specified index from a collection of child elements.
+        /// </summary>
+        /// <param name="index">The zero-based index of the requested child element in the collection.</param>
+        /// <returns>
+        /// The requested child element. This should not return null; if the provided index is out of range, an exception is thrown.
+        /// </returns>
+        protected override Visual GetVisualChild(int index)
+        {
+            return _child;
+        }
+
+        /// <summary>
+        /// Implements any custom measuring behavior for the adorner.
+        /// </summary>
+        /// <param name="constraint">A size to constrain the adorner to.</param>
+        /// <returns>
+        /// A <see cref="T:System.Windows.Size"/> object representing the amount of layout space needed by the adorner.
+        /// </returns>
+        protected override Size MeasureOverride(Size constraint)
+        {
+            if (PlaceholderReferenceElement != null && AdornedElement != null && AdornedElement.IsMeasureValid && !AreClose(PlaceholderReferenceElement.DesiredSize, AdornedElement.DesiredSize))
+            {
+                PlaceholderReferenceElement.InvalidateMeasure();
+            }
+            _child.Measure(constraint);
+            return _child.DesiredSize;
+        }
+
+        private static bool AreClose(double value1, double value2)
+        {
+            if (value1 == value2)
+            {
+                return true;
+            }
+            var num = ((Math.Abs(value1) + Math.Abs(value2)) + 10.0)*2.2204460492503131E-16;
+            var num2 = value1 - value2;
+            return ((-num < num2) && (num > num2));
+        }
+
+        private static bool AreClose(Size size1, Size size2)
+        {
+            return (AreClose(size1.Width, size2.Width) && AreClose(size1.Height, size2.Height));
+        }
+    }
+}
